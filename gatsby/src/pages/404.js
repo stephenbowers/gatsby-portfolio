86f60252912151a8f8d5b18053adc99d1fc26b11
Stephen Bowers <sb@thatstephenbowers.com>
2021-05-08 20:19:33 -0700

    Update 404 page

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import Layout from "../components/Layout"
+import styled from 'styled-components';
+import SEO from "../components/SEO";
+
+const FourOhFourStyles = styled.div`
+    place-items: center;
+    text-align: center;
+    font-size: 3rem;
+
+    @media (max-width: 700px) {
+        font-size: 2rem;
+    }
+
+    @media (max-width: 500px) {
+        font-size: 1.5rem;
+    }
+`;
 
 export default function FourOhFourPage() {
     return (
-        <Layout>
-            <p>404 Page</p>
-        </Layout>
+        <>
+            <SEO title={`Page Not Found`} />
+            <FourOhFourStyles>
+                <p>This is not a stop on the tour.</p>
+                <p>Please stick to the posted navigation.</p>
+            </FourOhFourStyles>
+        </>
     );
 }
