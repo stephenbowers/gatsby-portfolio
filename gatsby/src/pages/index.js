90addbb436bb9935c3290daa8fb26b29be477421
Stephen Bowers <sb@thatstephenbowers.com>
2021-05-10 21:19:32 -0700

    Add bowtie animation to home page

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import SEO from '../components/SEO';
+import BowTie from '../assets/images/BowTie.svg';
 
 const HomeStyles = styled.div`
-  
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+  place-content: center;
+
   h1, h2 {
     text-align: center;
     color: var(--onyx);
@@ -19,6 +25,36 @@ const HomeStyles = styled.div`
     font-size: 4rem;
   }
 
+  .bow-tie {
+    max-width: 600px;
+    animation: tilt 2s linear alternate-reverse infinite;
+  }
+
+  .image-container {
+    display: flex;
+    place-content: center;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  @keyframes tilt {
+    0% {
+      transform: rotate(-10deg)
+    }
+    50% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(10deg);
+    }
+  }
+
+  @media (max-width: 700px) {
+    .bow-tie {
+      max-width: 400px;
+    }
+  }
+
   @media (max-width: 600px) {
     h1 {
       font-size: 4rem;
@@ -28,6 +64,14 @@ const HomeStyles = styled.div`
       font-size: 2rem;
     }
   }
+
+  @media (max-width: 500px) {
+    .bow-tie {
+      max-width: 300px;
+    }
+  }
+
+
 `;
 
 export default function Home() {
@@ -37,6 +81,9 @@ export default function Home() {
       <HomeStyles>
         <h1>Stephen Bowers</h1>
         <h2>Web Developer</h2>
+        <div className="image-container">
+          <img className="bow-tie" src={BowTie} alt="bowtie" />
+        </div>
       </HomeStyles>
     </>
   );
